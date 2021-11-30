20d2363ec3b7d6dbd964f6581d036ebe8ee5e9bc
Jbernat88 <91455587+Jbernat88@users.noreply.github.com>
2021-11-30 12:58:30 +0100

    jjjj

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRigidbody;
+    [SerializeField] private float jumpForce = 400f;
+    public float gravityModifier = 1f;
+    private bool isOnTheGround = true
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRigidbody.AddForce(Vector3.up * jumpForce);
+            isOnTheGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision otherCollision)
+    {
+        IsOnTheGround = true;
+    }
+}
